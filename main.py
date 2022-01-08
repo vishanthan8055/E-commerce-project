7643c22374804d33a5601a2ca19f9b2db3572c7a
Vishanthan <vishanthan8055@gmail.com>
2022-01-08 21:39:14 +0530

    remove spaces

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ from bs4 import BeautifulSoup
 import time
 from notifypy import Notify
 
-
 product_url = input("Enter the Product Url:")
 price_list = []
 class_text = "_30jeq3 _16Jk6d"
+
 def get_price():
-
     getpage= requests.get(product_url)
-
     soup= BeautifulSoup(getpage.text, 'html.parser')
-
     price_tag = soup.find(class_ = class_text)
-
     price = (price_tag.get_text())
     price = float(price.replace(",", "").replace("₹", ""))
     price_list.append(price)
     return price
+
 def price_decrease_check(price_list):
     if price_list[-1] < price_list[-2]:
         return True
@@ -26,7 +23,6 @@ def price_decrease_check(price_list):
         return False
 
 count = 1
-
 
 def notify():
     notification = Notify()
